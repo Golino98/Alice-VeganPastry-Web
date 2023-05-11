955c151b3eb8bbe1946afae7e6c3615d04f05bee
Golino98 <giacomogolino@gmail.com>
2023-05-11 20:53:27 +0200

    Aggiunta autenticazione

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
 class FrontController extends Controller
 {
     public function getHome()
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [FrontController::class, 'getHome'])->name('home');
+
+//Rotta che mi permette di andare nella pagina di login
+Route::get('/user/login', [AuthController::class, 'authentication'])->name('user.login');
+Route::post('/user/login', [AuthController::class, 'login'])->name('user.login');
+Route::post('/user/register', [AuthController::class, 'registration'])->name('user.register');
+Route::get('/user/logout', [AuthController::class, 'logout'])->name('user.logout');
