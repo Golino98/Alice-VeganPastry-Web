839f8f6db00eaa0d49e290b8b6ec3c2d15f31ec1
Golino98 <giacomogolino@gmail.com>
2024-05-29 00:21:39 +0200

    risolto problema eliminazione dolci

--- resources/views/payment/success.blade.php
+++ resources/views/payment/success.blade.php
@@ -13,7 +13,7 @@
                     <lottie-player src="https://assets6.lottiefiles.com/packages/lf20_sb8d3ngn.json"  background="transparent" speed="1.5"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
                     <a class="btn btn-log shadow" role="button" href="{{route('home')}}"><i class="bi bi-balloon"></i> Pagina iniziale</a>
                     <div><br></div>
-                    <a class="btn btn-back" role="button" href="{{route('user.orders')}}"><i class="bi bi-bag-heart""></i> I miei ordini</a>
+                    <a class="btn btn-minus" role="button" href="{{route('user.orders')}}"><i class="bi bi-bag-heart""></i> I miei ordini</a>
                 </div>
             </div>
         </div>
